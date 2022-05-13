74239323de69001d120de24181ce5e0dbc1230a3
SakuraWh <sakurawangh@163.com>
2022-05-13 09:14:25 +0800

    2022.5.13

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # LeetCode
 This is my private leetcode repository.
+
+
+**文件命名规范：**
+来源-文件名
+
+来源：daily,competition,core，template,other(每日，竞赛，范例，模板，其他)
---
+++ src/main/middle/Daily_OneAwayLcci.java
@@ -0,0 +1,13 @@
+package main.middle;
+
+/**
+ * 双指针，字符串
+ */
+public class Daily_OneAwayLcci {
+    public boolean oneEditAway(String first, String second) {
+
+
+
+        return false;
+    }
+}
